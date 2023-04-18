322622e3d04df3764a94bd84aa2d0aece22106e2
Panthari Panthong <panthari.panthong@gmail.com>
2023-04-18 16:34:43 +0200

    done

--- app.js
+++ app.js
@@ -19,4 +19,22 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beers: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      console.log(responseFromAPI);
+      res.render('random-beer', { randomBeer: responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
